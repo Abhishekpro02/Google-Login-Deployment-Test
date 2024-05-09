46a4c9703d66131477bbe5b56023120c53b2f90b
Abhishek Kumar <sahniak56@gmail.com>
2024-05-09 16:53:39 +0530

    add protected route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,22 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import User from "./pages/User";
+import { useAuthContext } from "./context/AuthContext";
+import ProtectedRoute from "./Protectedroute";
 
 const App = () => {
+  const { authUser } = useAuthContext();
+  const isAuthenticated = authUser !== null;
+  console.log(isAuthenticated);
+
   return (
     <BrowserRouter>
       <Routes>
+        <Route element={<ProtectedRoute isAuthenticated={isAuthenticated} />}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/users" element={<User />} />
+        </Route>
         <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/users" element={<User />} />
       </Routes>
     </BrowserRouter>
   );
