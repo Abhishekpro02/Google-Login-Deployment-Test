c6552c74ebf31f451817f8883b3ff2c0e6ea5ff1
Abhishek Kumar <sahniak56@gmail.com>
2024-05-09 17:36:10 +0530

    nav css

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -3,17 +3,23 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
+    <div style={styles.navbarContainer}>
+      <nav style={styles.navbar}>
+        <ul style={styles.navList}>
+          <li style={styles.navItem}>
+            <NavLink to="/" style={styles.navLink}>
+              Home
+            </NavLink>
           </li>
-          <li>
-            <NavLink to="/profile">Profile</NavLink>
+          <li style={styles.navItem}>
+            <NavLink to="/profile" style={styles.navLink}>
+              Profile
+            </NavLink>
           </li>
-          <li>
-            <NavLink to="/users">Users</NavLink>
+          <li style={styles.navItem}>
+            <NavLink to="/users" style={styles.navLink}>
+              Users
+            </NavLink>
           </li>
         </ul>
       </nav>
@@ -22,3 +28,27 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+const styles = {
+  navbarContainer: {
+    backgroundColor: "#430694",
+    textAlign: "center",
+    padding: "10px 5px",
+  },
+  navbar: {
+    display: "inline-block",
+  },
+  navList: {
+    listStyleType: "none",
+    padding: 0,
+    margin: 0,
+  },
+  navItem: {
+    display: "inline",
+    margin: "0 20px",
+  },
+  navLink: {
+    color: "#fff",
+    textDecoration: "none",
+  },
+};
